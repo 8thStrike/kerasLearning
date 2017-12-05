7b8ae6d4c5bdc14fffda15a64b48a47e7313506f
=8thStrike <=edouard7@gmail.com>
2017-12-05 17:21:47 +0100

    adding examples

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ dataset= numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 X=dataset[:,0:8]
 Y=dataset[:,8]
 
-print(X.shape)
-print(X.size)
-print(Y.shape)
-print(Y.size)
 
 #create models
 model=Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8,activation='relu'))
+model.add(Dense(12, input_dim=8, activation='sigmoid'))
+model.add(Dense(18,activation='sigmoid'))
+model.add(Dense(22,activation='relu'))
+model.add(Dense(22,activation='sigmoid'))
+model.add(Dense(18,activation='relu'))
+model.add(Dense(8,activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 
 #compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #fit the model
-#model.fit(X, Y, epochs=150, batch_size=10)
-
+model.fit(X, Y, epochs=1500, batch_size=40)
+print("model.get_weights")
+print(model.get_weights())
 #evaluate the model
-#scores=model.evaluate(X,Y)
-#print("\n%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
+scores=model.evaluate(X,Y)
+print("\n%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
+print(model.summary())
